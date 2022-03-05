2bea45c14318d37f597bce468ffa84344dc22c70
dly2424 <46138781+dly2424@users.noreply.github.com>
2022-03-05 05:12:55 -0500

    Updated to reflect new version of py_tale use.
    No longer need a bunch of asyncio.sleep calls.
    
    Added some comments

--- Examples/Py_Bot/Py_Bot.py
+++ Examples/Py_Bot/Py_Bot.py
@@ -1,6 +1,6 @@
 import asyncio, datetime, json
 from discord import Intents
-from discord.ext import commands
+from discord.ext import commands    # discord.py will no longer be maintained. I recommend looking into disnake: https://github.com/DisnakeDev/disnake
 from py_tale import Py_Tale
 
 class Vars:                 #This creates variables we can use anywhere
@@ -15,11 +15,11 @@ class Vars:                 #This creates variables we can use anywhere
         self.has_started = False
 
 
-bot = Py_Tale()
+bot = Py_Tale()        #Instance of Py_Tale stored as bot
 my_data = Vars()       #We can access all the variables in class 'Vars' with my_data now.
 
 try:
-    with open("./config.json") as f:
+    with open("./config.json") as f:            #Open config.json in the working directory to read from it
         config = f.read()
         my_data.config = json.loads(config)
         bot.config(client_id=my_data.config["client_id"],  # This function sets our credentials
@@ -37,7 +37,7 @@ except Exception as e:
     exit()
 
 try:
-    with open("./discord_info.json") as f:
+    with open("./discord_info.json") as f:      #Open discord_info.json in the working directory to read from it
         info = f.read()
         my_data.discord_info = json.loads(info)
 except FileNotFoundError:
@@ -87,7 +87,7 @@ async def on_playermove(data):                      #This function is called eve
             if username not in my_data.player_chunk_timer:
                 my_data.player_chunk_timer[username] = datetime.datetime.now() - datetime.timedelta(seconds=1)
             if "Cave Layer" in new_chunk:
-                if datetime.datetime.now() > my_data.player_chunk_timer[username] + datetime.timedelta(seconds=10):
+                if datetime.datetime.now() > my_data.player_chunk_timer[username] + datetime.timedelta(seconds=10):     #This limits 1 message every 10 seconds per player. Prevents spam.
                     my_data.player_chunk_timer[username] = datetime.datetime.now()
                     await bot.send_command_console(server_id, f"player message {username} '{new_chunk}'")   #Message the player in game what layer they're on!
                     await my_data.client.get_guild(my_data.discord_info[k]["guild_id"]).get_channel(my_data.discord_info[k]["chunk_channel"]).send(f"```{username} has entered {new_chunk}```")
@@ -99,9 +99,9 @@ async def on_message(message):
     if message.author == my_data.client.user:           #Make sure the bot doesn't respond to itself
         return
 
-    if message.content.startswith("!add server ") and message.author.id == 216680607156011011:
-        json_dict = json.loads(message.content.split(" ", maxsplit=2)[2])
-        if len(my_data.discord_info) == 0:
+    if message.content.startswith("!add server ") and message.author.id == 216680607156011011:  #Make sure only a certain user can use the command
+        json_dict = json.loads(message.content.split(" ", maxsplit=2)[2])                       #This command will add a discord server and ATT server for the bot to operate on
+        if len(my_data.discord_info) == 0:                                                      #!add server {json format you can find in discord_info.json template}
             my_data.discord_info = json_dict
         else:
             my_data.discord_info = my_data.discord_info | json_dict
@@ -111,34 +111,35 @@ async def on_message(message):
 
 
 @my_data.client.event
-async def on_ready(): #When the discord bot is fully ready
-    if not my_data.has_started:
+async def on_ready():               #When the discord bot is fully ready
+    if not my_data.has_started:     #This check is necessary because on_ready can be called multiple times. We don't want to reinitialize our ATT bot and create dupe tasks.
         my_data.has_started = True
         print("Discord bot logged in and ready.")
         print("Starting ATT bot")
-        asyncio.create_task(bot.run()) #start the bot
-        await bot.wait_for_ws()
-        invites = await bot.request_invites()  # Get all of our invites to servers on startup
+        asyncio.create_task(bot.run())                  # Start the bot
+        await bot.wait_for_ws()                         # Make sure we wait for the main websocket to start. 
+        invites = await bot.request_invites()           # Get all of our invites to servers on startup
         print("Accepting outstanding invites")
         for x in invites:
-            await bot.request_accept_invite(x['id'])  # Accept the invites indiscriminately
+            await bot.request_accept_invite(x['id'])    # Accept the invites indiscriminately
         await bot.main_sub(f"subscription/me-group-invite-create/{bot.user_id}", on_invited)   #subscribe to getting invites to groups (This lasts forever)
         print("Creating connections to all server consoles")
         for k in dict(my_data.discord_info):
-            await bot.create_console(my_data.discord_info[k]["att_server_id"])
-            print("loop")
-        await asyncio.sleep(10) #Ensure all consoles have time to open
-        print("Subscribing to events on all server consoles")
-        for k in dict(my_data.discord_info):
             try:
-                await asyncio.sleep(.1) #This is placed to prevent super fast spam to the server
+                await bot.create_console(my_data.discord_info[k]["att_server_id"])
+            except Exception as e:
+                print("Failed to create console with the following error:\n", e)
+        print("Subscribing to events on all server consoles")
+        for k in dict(my_data.discord_info):    #loop through a instanced copy of my_data.discord_info. To make sure we don't change size during iteration.
+            try:
+                await asyncio.sleep(.1)         #This is placed to prevent super fast spam to the server
                 await bot.console_sub("PlayerKilled", on_playerkilled, my_data.discord_info[k]["att_server_id"])
                 await bot.console_sub("PlayerMovedChunk", on_playermove, my_data.discord_info[k]["att_server_id"])
                 await bot.console_sub("PlayerJoined", on_playerjoin, my_data.discord_info[k]["att_server_id"])
                 await bot.console_sub("PlayerLeft", on_playerleft, my_data.discord_info[k]["att_server_id"])
             except Exception as e:
                 print(e)
-                print(f"Failed to sub to event for guild: '{k}'")
+                print(f"Failed to sub to event for guild: '{k}'")   #Just in case we get an error.
         print("Finished initialization. Bot is ready to roll!")
 
 my_data.client.run(my_data.token) #This runs our discord bot
